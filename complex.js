e007391333a2687565074bfcadbaff204f8ebf11
AndreBlanco <anrdrezinho.blanco@gmail.com>
2015-06-19 23:49:30 -0300

    REVISA O CÓDIGO, PIAUI, PARA VERIFICAR SE ESTÁ TUDO CERTO

--- complex.js
+++ complex.js
@@ -10,7 +10,8 @@ Complex.js - Complex numbers library for JavaScript
  * @param string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function getComplex(cNumber) {
+var Complex = Complex || {};
+Complex.get = function(cNumber) {
 	var complexArray = /([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)\s*([-+])?\s*([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)i/.exec(cNumber),
 	    complexNumber = /(^[-+]?\d+\.?\d*|^[-+]?\d*\.?\d+$)i/.exec(cNumber),
 	    complexAlt = /([-+]?\d+\.?\d*|[-+]?\d*\.?\d+)\s*([-+])?\s*i/.exec(cNumber);
@@ -18,7 +19,7 @@ function getComplex(cNumber) {
 		var obj = {
 			r: cNumber,
 			i: 0,
-			eq: eqComplex(cNumber, 0)
+			eq: Complex.eq(cNumber, 0)
 		};
 		return obj;
 	}
@@ -26,7 +27,7 @@ function getComplex(cNumber) {
 		var obj = {
 			r: 0,
 			i: parseFloat(complexNumber[1]),
-			eq: eqComplex(0, parseFloat(complexNumber[0]))
+			eq: Complex.eq(0, parseFloat(complexNumber[0]))
 		};
 		return obj;
 	}
@@ -35,7 +36,7 @@ function getComplex(cNumber) {
 		var obj = {
 			r: parseFloat(complexArray[1]),
 			i: parseFloat(complexArray[3]),
-			eq: eqComplex(parseFloat(complexArray[1]), parseFloat(complexArray[3]))
+			eq: Complex.eq(parseFloat(complexArray[1]), parseFloat(complexArray[3]))
 		};
 		return obj;
 	}
@@ -44,20 +45,20 @@ function getComplex(cNumber) {
 			obj = {
 				r: parseFloat(complexAlt[1]),
 				i: i,
-				eq: eqComplex(parseFloat(complexAlt[1]), parseFloat(i))
+				eq: Complex.eq(parseFloat(complexAlt[1]), parseFloat(i))
 			};
 		return obj;
 	}
 	else if (typeof cNumber == "object" && (cNumber.r || cNumber.i || cNumber.eq)) {
 		if (cNumber.eq) {
-			return getComplex(cNumber.eq);
+			return Complex.get(cNumber.eq);
 		}
 		else {
-			return getComplex(eqComplex(cNumber.r||"0", cNumber.i||"0"));
+			return Complex.get(eqComplex(cNumber.r||"0", cNumber.i||"0"));
 		}
 	}	
 	else if (typeof cNumber == "string" && cNumber == "i"){
-		return getComplex("1i");
+		return Complex.get("1i");
 	}
 	else{
 		return "Houston, we have a problem. Where's the complex?"
@@ -71,7 +72,7 @@ function getComplex(cNumber) {
  * @param b->number
  * @returns String((r)+(i)i)
  */
-function eqComplex(a, b) {
+Complex.eq = function(a, b) {
 	var plus = (b >= 0 ? "+" : "");
 	return String(a) + plus + String(b) + "i";
 }
@@ -83,11 +84,11 @@ function eqComplex(a, b) {
  * @param i->number
  * @returns Object(r, i, eq)
  */
-function complexObj(r, i) {
+Complex.obj = function(r, i) {
 	var obj = {
 		r: r,
 		i: i,
-		eq: eqComplex(r, i)
+		eq: Complex.eq(r, i)
 	};
 	return obj;
 }
@@ -99,12 +100,12 @@ function complexObj(r, i) {
  * @param b-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function sumComplex(a, b) {
-	var a = getComplex(a),
-		b = getComplex(b),
+Complex.sum = function(a, b) {
+	var a = Complex.get(a),
+		b = Complex.get(b),
 		r = a.r + b.r,
 		i = a.i + b.i;
-	return complexObj(r, i);
+	return Complex.obj(r, i);
 }
 /**
  * Return the subtraction of a and b 
@@ -114,12 +115,12 @@ function sumComplex(a, b) {
  * @param b-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function subtraComplex(a, b) {
-	var a = getComplex(a),
-		b = getComplex(b),
+Complex.subtra = function(a, b) {
+	var a = Complex.get(a),
+		b = Complex.get(b),
 		r = a.r - b.r,
 		i = a.i - b.i;
-	return complexObj(r, i);
+	return Complex.obj(r, i);
 }
 /**
  * Return the multiplication of a and b 
@@ -129,12 +130,12 @@ function subtraComplex(a, b) {
  * @param b-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function multipliComplex(a, b) {
-	var a = getComplex(a),
-		b = getComplex(b),
+Complex.multipli = function(a, b) {
+	var a = Complex.get(a),
+		b = Complex.get(b),
 		r = a.r * b.r - (a.i * b.i),
 		i = a.r * b.i + a.i * b.r;
-	return complexObj(r, i);
+	return Complex.obj(r, i);
 }
 /**
  * Return the division of a and b 
@@ -144,12 +145,12 @@ function multipliComplex(a, b) {
  * @param b-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function divComplex(a, b) {
-	var a = getComplex(a),
-		b = getComplex(b),
+Complex.div = function(a, b) {
+	var a = Complex.get(a),
+		b = Complex.get(b),
 		r = (a.r * b.r + a.i * b.i) / (b.r * b.r + b.i * b.i),
 		i = (b.r * a.i - a.r * b.i) / (b.r * b.r + b.i * b.i);
-	return complexObj(r, i);
+	return Complex.obj(r, i);
 }
 /**
  * Return modulus of the complex number
@@ -158,8 +159,8 @@ function divComplex(a, b) {
  * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Number
  */
-function modComplex(a) {
-	var a = getComplex(a),
+Complex.mod = function(a) {
+	var a = Complex.get(a),
 		modZ = Math.hypot(a.r, a.i);
 	return modZ;
 }
@@ -170,11 +171,11 @@ function modComplex(a) {
  * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Number
  */
-function cosComplex(a) {
-	var a = getComplex(a),
-	cosComplexR = Math.cos(a.r)*Math.cosh(a.i),
-	cosComplexI = -(Math.sin(a.r)*Math.sinh(a.i));
-	return complexObj(cosComplexR, cosComplexI);
+Complex.cos = function(a) {
+	var a = Complex.get(a),
+		cosComplexR = Math.cos(a.r)*Math.cosh(a.i),
+		cosComplexI = -(Math.sin(a.r)*Math.sinh(a.i));
+	return Complex.obj(cosComplexR, cosComplexI);
 }
 /**
  * Return sin of the complex number
@@ -183,17 +184,25 @@ function cosComplex(a) {
  * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Number
  */
-function sinComplex(a) {
-	var a = getComplex(a),
-	sinComplexR = Math.sin(a.r) * Math.cosh(a.i),
-	sinComplexI = Math.cos(a.r) * Math.sinh(a.i);
-	return complexObj(sinComplexR, sinComplexI);
+Complex.sin = function(a) {
+	var a = Complex.get(a),
+		sinComplexR = Math.sin(a.r) * Math.cosh(a.i),
+		sinComplexI = Math.cos(a.r) * Math.sinh(a.i);
+	return Complex.obj(sinComplexR, sinComplexI);
 }
-function tanComplex(a){
-	var a = getComplex(a),
-	tanComplexR = divComplex(sinComplex(a), cosComplex(a)).r,
-	tanComplexI = divComplex(sinComplex(a), cosComplex(a)).i;
-	return complexObj(tanComplexR, tanComplexI);
+/**
+ * Return tan of the complex number
+ * @author Lucas Vasconcelos <lucaslg26@yahoo.com.br> and André Blanco <andrezinho.blanco@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
+ * @returns Number
+ */
+
+Complex.tan = function(a){
+	var a = Complex.get(a),
+		tanComplexR = divComplex(sinComplex(a), cosComplex(a)).r,
+		tanComplexI = divComplex(sinComplex(a), cosComplex(a)).i;
+	return Complex.obj(tanComplexR, tanComplexI);
 }
 /**
  * Return trigonometric form of the complex number
@@ -202,13 +211,13 @@ function tanComplex(a){
  * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function trigonoFormComplex(a) {
-	var a = getComplex(a),
-		plus = ((a.i/modComplex(a)) >= 0 ? "+" : ""),
+Complex.trigonoForm = function(a) {
+	var a = Complex.get(a),
+		plus = ((a.i/Complex.mod(a)) >= 0 ? "+" : ""),
 		obj = {
-			r: modComplex(a) * (a.r/modComplex(a)),
-			i: modComplex(a) * (a.i/modComplex(a)),
-			eq: String(modComplex(a)) + "(" + (a.r/modComplex(a)) + plus + (a.i/modComplex(a)) + "i" + ")"
+			r: Complex.mod(a) * (a.r/Complex.mod(a)),
+			i: Complex.mod(a) * (a.i/Complex.mod(a)),
+			eq: String(Complex.mod(a)) + "(" + (a.r/Complex.mod(a)) + plus + (a.i/Complex.mod(a)) + "i" + ")"
 		};
 	return obj;
 }
@@ -219,11 +228,11 @@ function trigonoFormComplex(a) {
  * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function logComplex(a) {
-	var a = getComplex(a),
-	logR = Math.log(modComplex(a)),
-	argZ = Math.atan2(a.i, a.r);
-	return complexObj(logR, argZ);
+Complex.log = function(a) {
+	var a = Complex.get(a),
+		logR = Math.log(Complex.mod(a)),
+		argZ = Math.atan2(a.i, a.r);
+	return Complex.obj(logR, argZ);
 }
 /**
  * Return the log of the complex number with other base 
@@ -233,12 +242,12 @@ function logComplex(a) {
  * @param b-> Number(base)
  * @returns Object(r, i, eq)
  */
-function logBaseComplex(a, b) {
-	var a = logComplex(a),
-	b = Math.log(b),
-	logBaseComplexR = a.r / b,
-	logBaseComplexI = a.i / b;
-	return complexObj(logBaseComplexR, logBaseComplexI);
+Complex.logBase = function(a, b) {
+	var a = Complex.log(a),
+		b = Math.log(b),
+		logBaseComplexR = a.r / b,
+		logBaseComplexI = a.i / b;
+	return Complex.obj(logBaseComplexR, logBaseComplexI);
 }
 /**
  * Return sinh of the complex number
@@ -247,11 +256,11 @@ function logBaseComplex(a, b) {
  * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function sinhComplex(a) {
-	var a = getComplex(a),
+Complex.sinh = function(a) {
+	var a = Complex.get(a),
 		sinhComplexR = (Math.pow(Math.E, a.r) * Math.cos(a.i) - Math.pow(Math.E, -a.r) * Math.cos(a.i)) / 2,
 		sinhComplexI = (Math.pow(Math.E, a.r) * Math.sin(a.i) + Math.pow(Math.E, -a.r) * Math.sin(a.i)) / 2;
-	return complexObj(sinhComplexR, sinhComplexI);
+	return Complex.obj(sinhComplexR, sinhComplexI);
 }
 /**
  * Return cosh of the complex number
@@ -260,11 +269,11 @@ function sinhComplex(a) {
  * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function coshComplex(a) {
-	var a = getComplex(a),
+Complex.cosh = function(a) {
+	var a = Complex.get(a),
 		coshComplexR = (Math.pow(Math.E, a.r) * Math.cos(a.i) + Math.pow(Math.E, -a.r) * Math.cos(a.i)) / 2,
 		coshComplexI = (Math.pow(Math.E, a.r) * Math.sin(a.i) - Math.pow(Math.E, -a.r) * Math.sin(a.i)) / 2;
-	return complexObj(coshComplexR, coshComplexI);
+	return Complex.obj(coshComplexR, coshComplexI);
 }
 /**
  * Return tanh of the complex number
@@ -273,88 +282,138 @@ function coshComplex(a) {
  * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function tanhComplex(a){
-	var a = getComplex(a),
-      tanhComplexR = divComplex(sinhComplex(a), coshComplex(a)).r,
-      tanhComplexI = divComplex(sinhComplex(a), coshComplex(a)).i;
-  return complexObj(tanhComplexR, tanhComplexI);
+Complex.tanh = function(a){
+	var a = Complex.get(a),
+      		tanhComplexR = Complex.div(Complex.sinh(a), Complex.cosh(a)).r,
+      		tanhComplexI = Complex.div(Complex.sinh(a), Complex.cosh(a)).i;
+  return Complex.obj(tanhComplexR, tanhComplexI);
 }
 /**
- * Return *
+ * Return a complex number high the other number complex or natural number
  * @author Lucas Vasconcelos <lucaslg26@yahoo.com.br> and André Blanco <andrezinho.blanco@gmail.com>
  * @license http://www.apache.org/licenses/LICENSE-2.0
  * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
  * @returns Object(r, i, eq)
  */
-function expComplex(a, c){
-	var a = getComplex(a),
-      c = getComplex(c),
-      expR = Math.pow(Math.E, (c.r * logComplex(a).r - c.i * logComplex(a).i)) * Math.cos(c.r * logComplex(a).i + c.i * logComplex(a).r),
-      expI = Math.pow(Math.E, (c.r * logComplex(a).r - c.i * logComplex(a).i)) * Math.sin(c.r * logComplex(a).i + c.i * logComplex(a).r);
-  return complexObj(expR, expI);
+Complex.exp = function(a, c){
+	var a = Complex.get(a),
+      		c = Complex.get(c),
+      		expR = Math.pow(Math.E, (c.r * Complex.log(a).r - c.i * Complex.log(a).i)) * Math.cos(c.r * Complex.log(a).i + c.i * Complex.log(a).r),
+      		expI = Math.pow(Math.E, (c.r * Complex.log(a).r - c.i * Complex.log(a).i)) * Math.sin(c.r * Complex.log(a).i + c.i * Complex.log(a).r);
+  	return Complex.obj(expR, expI);
 }
-function rtComplex(a, n){
-	var a = getComplex(a);
+/**
+ * return the roots of complex number, according to the degree of root (second parameter)
+ * @author Lucas Vasconcelos <lucaslg26@yahoo.com.br> and André Blanco <andrezinho.blanco@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
+ * @returns Object(r, i, eq)
+ */
+Complex.root = function(a, n){
+	var a = Complex.get(a);
 	var roots=[];
 	for (var z=0; z<n; z++){
-		var rtComplexI = Math.pow(modComplex(a), 1/n) * Math.cos(Math.atan(a.i/a.r)/n + (2 * z * Math.PI)/n),
-		rtComplexR = Math.pow(modComplex(a), 1/n) * Math.sin(Math.atan(a.i/a.r)/n + (2 * z * Math.PI)/n);
-		roots.push(complexObj(rtComplexI, rtComplexR));
+		var rtComplexI = Math.pow(Complex.mod(a), 1/n) * Math.cos(Math.atan2(a.i,a.r)/n + (2 * z * Math.PI)/n),
+			rtComplexR = Math.pow(Complex.mod(a), 1/n) * Math.sin(Math.atan2(a.i,a.r)/n + (2 * z * Math.PI)/n);
+			roots.push(Complex.obj(rtComplexI, rtComplexR));
 	}
 	return roots;
 }
-function asinComplex(a){
-	var a = getComplex(a),
-	b = multipliComplex(a,'1i').eq,
-	c = multipliComplex(a,a).eq,
-	d = subtraComplex('1+0i',c).eq,
-	e = expComplex(d, 0.5).eq,
-	f = sumComplex(b,e).eq,
-	asinComplexR = logComplex(f).i,
-	asinComplexI = -1 * logComplex(f).r;
-	return complexObj(asinComplexR, asinComplexI);
+/**
+ * return an asin of the complex number 
+ * @author Lucas Vasconcelos <lucaslg26@yahoo.com.br> and André Blanco <andrezinho.blanco@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
+ * @returns Object(r, i, eq)
+ */
+Complex.asin = function(a){
+	var a = Complex.get(a),
+		b = Complex.multipli(a,'1i'),
+		c = Complex.multipli(a,a),
+		d = Complex.subtra('1+0i',c),
+		e = Complex.exp(d, 0.5),
+		f = Complex.sum(b,e),
+		asinComplexR = Complex.log(f).i,
+		asinComplexI = -1 * Complex.log(f).r;
+	return Complex.obj(asinComplexR, asinComplexI);
 }
-function acosComplex(a){
-	var a = getComplex(a),
-	b = multipliComplex(a,a),
-	c = subtraComplex(b, 1),
-	d = expComplex(c, 0.5),
-	e = sumComplex(a, d),
-	acosComplexR = logComplex(e).i,
-	acosComplexI = -1 * logComplex(e).r;
-	return complexObj(acosComplexR, acosComplexI);
+/**
+ * return an acos of the complex number 
+ * @author Lucas Vasconcelos <lucaslg26@yahoo.com.br> and André Blanco <andrezinho.blanco@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
+ * @returns Object(r, i, eq)
+ */
+
+Complex.acos = function(a){
+	var a = Complex.get(a),
+		b = Complex.multipli(a,a),
+		c = Complex.subtra(b, 1),
+		d = Complex.exp(c, 0.5),
+		e = Complex.sum(a, d),
+		acosComplexR = Complex.log(e).i,
+		acosComplexI = -1 * Complex.log(e).r;
+	return Complex.obj(acosComplexR, acosComplexI);
 }
-function atanComplex(a){
-	var a = getComplex(a),
-	b =  multipliComplex('0.5i', logComplex(divComplex(sumComplex('i', a), subtraComplex('i', a)))),
-	atanComplexR = b.r,
-	atanComplexI = b.i;
-	return complexObj(atanComplexR, atanComplexI);
+/**
+ * return an atan of the complex number 
+ * @author Lucas Vasconcelos <lucaslg26@yahoo.com.br> and André Blanco <andrezinho.blanco@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
+ * @returns Object(r, i, eq)
+ */
+Complex.atan = function(a){
+	var a = Complex.get(a),
+		b =  Complex.multipli('0.5i', Complex.log(Complex.div(Complex.sum('i', a), Complex.subtra('i', a)))),
+		atanComplexR = b.r,
+		atanComplexI = b.i;
+	return Complex.obj(atanComplexR, atanComplexI);
 }
-function asinhComplex(a){
-	var a = getComplex(a),
-	b = expComplex(a,2),
-	c = sumComplex(b, 1),
-	d = expComplex(c, 0.5),
-	e = sumComplex(a, d),
-	asinhComplexR = logComplex(e).r,
-	asinhComplexI = logComplex(e).i;
-	return complexObj(asinhComplexR, asinhComplexI);
+/**
+ * return an asinh of the complex number 
+ * @author Lucas Vasconcelos <lucaslg26@yahoo.com.br> and André Blanco <andrezinho.blanco@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
+ * @returns Object(r, i, eq)
+ */
+Complex.asinh = function(a){
+	var a = Complex.get(a),
+		b = Complex.exp(a,2),
+		c = Complex.sum(b, 1),
+		d = Complex.exp(c, 0.5),
+		e = Complex.sum(a, d),
+		asinhComplexR = Complex.log(e).r,
+		asinhComplexI = Complex.log(e).i;
+	return Complex.obj(asinhComplexR, asinhComplexI);
 }
-function acoshComplex(a){
-	var a = getComplex(a),
-	b = expComplex(a,2),
-	c = subtraComplex(b, 1),
-	d = expComplex(c, 0.5),
-	e = sumComplex(a, d),
-	acoshComplexR = logComplex(e).r,
-	acoshComplexI = logComplex(e).i;
-	return complexObj(acoshComplexR, acoshComplexI);
+/**
+ * return an acosh of the complex number 
+ * @author Lucas Vasconcelos <lucaslg26@yahoo.com.br> and André Blanco <andrezinho.blanco@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
+ * @returns Object(r, i, eq)
+ */
+Complex.acosh = function(a){
+	var a = Complex.get(a),
+		b = Complex.exp(a,2),
+		c = Complex.subtra(b, 1),
+		d = Complex.exp(c, 0.5),
+		e = Complex.sum(a, d),
+		acoshComplexR = Complex.log(e).r,
+		acoshComplexI = Complex.log(e).i;
+	return Complex.obj(acoshComplexR, acoshComplexI);
 }
-function atanhComplex(a){
-	var a = getComplex(a),
-	b =  multipliComplex(0.5, logComplex(divComplex(sumComplex(1, a), subtraComplex(1, a)))),
-	atanhComplexR = b.r,
-	atanhComplexI = b.i;
-	return complexObj(atanhComplexR, atanhComplexI);
+/**
+ * return an atanh of the complex number 
+ * @author Lucas Vasconcelos <lucaslg26@yahoo.com.br> and André Blanco <andrezinho.blanco@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @param a-> string(literal expression, complex number), numbers(real part) and object(r, i, eq)
+ * @returns Object(r, i, eq)
+ */
+Complex.atanh = function(a){
+	var a = Complex.get(a),
+		b =  Complex.multipli(0.5, Complex.log(Complex.div(Complex.sum(1, a), Complex.subtra(1, a)))),
+		atanhComplexR = b.r,
+		atanhComplexI = b.i;
+	return Complex.obj(atanhComplexR, atanhComplexI);
 }
